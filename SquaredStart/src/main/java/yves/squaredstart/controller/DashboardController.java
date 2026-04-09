d2facaa3373318ea3b2e971f60f059c3a2a2bbeb
andreselianor <113841548+andreselianor@users.noreply.github.com>
2026-04-09 18:14:32 +0200

    feat: first drafts of landing page / html fragments / added new component1 images

--- SquaredStart/src/main/java/yves/squaredstart/controller/DashboardController.java
+++ SquaredStart/src/main/java/yves/squaredstart/controller/DashboardController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class DashboardController {
     @GetMapping("")
     public String dashboard(){
-        return "dashboard";
+        return "/user/dashboard";
     }
 }
--- SquaredStart/src/main/java/yves/squaredstart/controller/LandingController.java
+++ SquaredStart/src/main/java/yves/squaredstart/controller/LandingController.java
@@ -1,14 +1,25 @@
 package yves.squaredstart.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Random;
 
 @Controller
 @RequestMapping("/")
 public class LandingController {
     @GetMapping("")
-    public String hello(){
-        return "landing";
+    public String landingPage(Model model) {
+        List<String> imagenes = List.of("componenteColinas.png", "componenteDesierto.png", "componenteArtico.png","componenteJungla.png");
+
+        Random random = new Random();
+        String imagenRandom = imagenes.get(random.nextInt(imagenes.size()));
+
+        model.addAttribute("image", imagenRandom);
+
+        return "landingPage";
     }
 }
